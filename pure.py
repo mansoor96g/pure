5f7942a4de337980159cdb3f4926e996e04c766d
AJ <anil@aniljava.com>
2015-09-15 00:27:34 -0600

    Added toid

--- pure.py
+++ pure.py
@@ -4,3 +4,23 @@ def connect(path):
     connection = sqlite3.connect(path)
     connection.row_factory = sqlite3.Row
     connection.text_factory = str
+
+
+def toid(text, delim='-'):
+    if text is None:
+        return ''
+
+    text = delim + text + delim
+    result = []
+    text = text.lower()
+
+    for c in text:
+        if c in '0123456789abcdefghijklmnopqrstuvwxyz':
+            result.append(c)
+        else:
+            result.append(delim)
+
+    result = ''.join(result)
+    while (delim+delim in result):
+        result = result.replace(delim+delim, delim)
+    return result[1:-1]
